a72d6508c07ab993a44414b2bc93dcb3fc9e7b3d
soundobj <soundobj@yahoo.com>
2015-02-14 17:04:29 +0000

    traceur installation
    from:
    http://weblogs.asp.net/dwahlin/getting-started-with-es6-%E2%80%93-transp
    iling-es6-to-es5
    
    TODO: create gulp task to watch es6 code

---
+++ js/es6/neon.js
@@ -0,0 +1,9 @@
+// neon.js
+class Neon {
+  sayHi(name = 'Anonymous') {
+    console.log(`Hi neon ${name}!`);
+  }
+}
+
+var neon = new Neon();
+neon.sayHi();
--- js/neon.js
+++ js/neon.js
@@ -1,7 +1,13 @@
-console.log("neon.js loaded");
-
-
-$("#O").find("[id^='end']").each(function(){
-	console.log(this.id);
-	$("#" + this.id).attr("class","tube");
+System.registerModule("es6/neon.js", [], function() {
+  "use strict";
+  var __moduleName = "es6/neon.js";
+  var Neon = function Neon() {};
+  ($traceurRuntime.createClass)(Neon, {sayHi: function() {
+      var name = arguments[0] !== (void 0) ? arguments[0] : 'Anonymous';
+      console.log(("Hi neon " + name + "!"));
+    }}, {});
+  var neon = new Neon();
+  neon.sayHi();
+  return {};
 });
+System.get("es6/neon.js" + '');
