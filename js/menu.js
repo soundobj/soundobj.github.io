48341d872bd978d08e14c6f1fa70e5474501b461
soundobj <soundobj@yahoo.com>
2015-03-08 09:44:36 +0000

    toggle slide menu
    change settings icon color

--- js/menu.js
+++ js/menu.js
@@ -6,22 +6,28 @@
 	'use strict';
 	var	mask = document.createElement("div");
 	mask.className = "mask";
-	var	toggleSlideLeft = document.querySelector( ".menu-icon" );
 	var	activeNav;
-	
-	/* slide menu left */
-	toggleSlideLeft.addEventListener( "click", function(){
-		console.log("click lieft 6");
-		$("body").addClass("sml-open");		
-		document.body.appendChild(mask);
-		activeNav = "sml-open";
-		$("#settings-icon").toggleClass("settings-icon-closed settings-icon-open");
-	} );
 
-	/* hide active menu if mask is clicked */
-	mask.addEventListener( "click", function(){
+	$(".menu-icon").click(function(){
+		if($(".mask").length) {
+			console.log("got to close");
+			hideMenu();
+			$("#settings-icon").attr("class","settings-icon-closed");
+		} else {
+			console.log("got to open");
+			$("body").addClass("sml-open");		
+			document.body.appendChild(mask);
+			activeNav = "sml-open";
+			$("#settings-icon").attr("class","settings-icon-open");
+		}
+	});
+
+	function hideMenu(){
 		$("body").removeClass(activeNav);
 		activeNav = "";
 		document.body.removeChild(mask);		
-	} );
+	}
+
+	/* hide active menu if mask is clicked */
+	mask.addEventListener( "click", hideMenu);
 })( window );
